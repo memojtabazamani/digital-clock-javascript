b464040798ff16625b5a15095bdbaabaca375332
memojtabazamani <memojtabazamani@gmail.com>
2021-07-11 16:04:12 +0430

    done; working in digital clock

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
 var nowDate = new Date();
 
-var oldHour    = nowDate.getHours();
-var oldMinutes = nowDate.getMinutes();
-var oldSeconds = nowDate.getSeconds();
-
-if(oldSeconds <  10) {
-    oldSeconds = "0" + oldSeconds;
-}
-if(oldMinutes < 10) {
-    oldMinutes = "0" + oldMinutes;
-}
-if(oldHour < 10) {
-    oldHour = "0" + oldHour;
-}
+var oldHour    =  (nowDate.getHours() <10 ?'0':'') + nowDate.getHours();
+var oldMinutes = (nowDate.getMinutes() <10 ?'0':'') + nowDate.getMinutes();
+var oldSeconds = (nowDate.getSeconds() <10 ?'0':'') + nowDate.getSeconds() ;
 
 const hours_1 = document.getElementById("hours_1");
 const hours_2 = document.getElementById("hours_2");
@@ -23,9 +13,6 @@ const minutes_2 = document.getElementById("minutes_2");
 const seconds_1 = document.getElementById("second_1");
 const seconds_2 = document.getElementById("second_2");
 
-oldSeconds = oldSeconds.toString();
-oldMinutes = oldMinutes.toString();
-oldHour    = oldHour.toString();
 
 seconds_1.innerHTML = oldSeconds[0];
 seconds_2.innerHTML = oldSeconds[1];
@@ -38,54 +25,29 @@ hours_2.innerHTML   = oldHour[1];
 
 function setTime() {
     oldSeconds++;
-    if(oldSeconds < 10) { // Check be a
-        oldSeconds = "0" + oldSeconds;
-    }
+    oldSeconds = (oldSeconds < 10 ?'0':'') + oldSeconds;
     if(oldSeconds == 60) {
         oldSeconds = "00";
         oldMinutes++;
-        if(oldMinutes < 10) {
-            oldMinutes = "0" + oldMinutes;
-        }
+        oldMinutes = (oldMinutes < 10 ?'0':'') + oldMinutes;
     }
     if(oldMinutes == 60) {
         oldMinutes = "00";
         oldHour++;
-        if(oldHour < 10) {
-            oldHour = "0" + oldHour;
-        }
+        oldHour = (oldHour < 10 ? '0' : '') + oldHour;
     }
     if(oldHour == 24) {
         oldHour = "00";
     }
-    oldSeconds = oldSeconds.toString();
     seconds_1.innerHTML = oldSeconds[0];
     seconds_2.innerHTML = oldSeconds[1];
 
-
-    oldMinutes = oldMinutes.toString()
     minutes_1.innerHTML = oldMinutes[0];
     minutes_2.innerHTML = oldMinutes[1];
 
-
-    oldHour    = oldHour.toString();
     hours_1.innerHTML   = oldHour[0];
     hours_2.innerHTML   = oldHour[1];
-}
-
-
-function setNewTime() {
-    console.log("Seconds : "  + oldSeconds);
-    console.log("Minutes : " + oldMinutes);
-    console.log("Hour : " + oldHour);
-
-
-
-
-
-
-
-
 
 }
+
 setInterval(setTime, 1000);
